517cfe3afbedb9a505356704df55ea07d31071a9
Aurel Wildfellner <aurel.w@gmail.com>
2015-03-27 23:03:17 +0100

    fix firmware, baudrate, devide by 0 in servolib.

--- firmware/winkekatze/winkekatze.ino
+++ firmware/winkekatze/winkekatze.ino
@@ -11,7 +11,11 @@ int pos = 0;    // variable to store the servo position
 void setup()
 {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+<<<<<<< HEAD
+  Serial.begin(9600);
+=======
   Serial.begin(19200);
+>>>>>>> 3e70bb536d119ee8a5a5a5e296e6d53ee2b5ce45
   goHome();
 }
 
@@ -24,7 +28,11 @@ void goHome() {
 void waveOnce(int wait) {
   int startPos = pos;
  
+<<<<<<< HEAD
+  for(pos = 1; pos < maxPos; pos += 3)  // goes from 0 degrees to 180 degrees
+=======
   for(pos = 0; pos < maxPos; pos += 3)  // goes from 0 degrees to 180 degrees
+>>>>>>> 3e70bb536d119ee8a5a5a5e296e6d53ee2b5ce45
   {                                  // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(3*wait);                       // waits 15ms for the servo to reach the position
